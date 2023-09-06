a27576c1e39e3b3fd6cdecaf9b3b1a13d85f9617
Lê Thanh Hùng <73764342+ThanhHung2112@users.noreply.github.com>
2023-09-07 01:03:19 +0700

    Login

--- fanpage_bot/get_user_from_fanpage.py
+++ fanpage_bot/get_user_from_fanpage.py
@@ -4,9 +4,13 @@ from selenium.webdriver.chrome.service import Service
 
 from time import sleep
 import random
-class Login():
+
+from login_fb import Login
+
+
+class GET_USER_FFANPAGE():
     def __init__(self):
-        
+
         self.username = "yelan482@gmail.com"
         self.password = "Linhcute542002"
         # Options
@@ -20,6 +24,8 @@ class Login():
         options.add_argument("--window-size=1920x1080")
 
         self.browser = Chrome(service=service, options=options)
+        # self.browser = Login().login()
+
     def login(self):
 
         self.browser.get("https://www.facebook.com/")
@@ -31,8 +37,19 @@ class Login():
 
         self.browser.find_element(By.NAME, "login").click()
         sleep(random.randint(3,5))
+       
 
         return
     
-Login().login()  
+    def get_user_link(self):
+
+        return self.browser.get("https://www.facebook.com/DoantruongQuocHoc")
+
+        
+    
+
+G = GET_USER_FFANPAGE()
+G.login()
+G.get_user_link()
+# G.runs()
 
---
+++ fanpage_bot/run.py
@@ -0,0 +1,10 @@
+from get_user_from_fanpage import GET_USER_FFANPAGE
+from login_fb import Login
+import time
+
+if __name__ == '__main__':
+
+    g = GET_USER_FFANPAGE()
+    g.login()
+    g.get_user_links()
+    # g.browser.close()
