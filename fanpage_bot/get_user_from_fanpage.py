fb2d3eeb5e09a77d68e6eedc279a897b36b3e04e
Lê Thanh Hùng <73764342+ThanhHung2112@users.noreply.github.com>
2024-01-10 10:50:04 +0700

    update xpath

--- fanpage_bot/get_user_from_fanpage.py
+++ fanpage_bot/get_user_from_fanpage.py
@@ -7,20 +7,20 @@ import re
 import pandas as pd
 from time import sleep
 import random
+import csv
 
 # from login_fb import Login
 
 
-class GET_USER_FFANPAGE():
+class GET_USER_FFANPAGE:
     def __init__(self):
-
-        self.username = "Buithingan011195@gmail.com"  # "yelan482@gmail.com"
-        self.password = "iubele"  # "Linhcute542002"
+        self.username =  "yelan482@gmail.com" #"Buithingan011195@gmail.com"  #
+        self.password =  "Linhcute542002" # "iubele"  #
 
         self.df = pd.read_csv("datasets/data.csv")
         # Options
 
-        service = Service(executable_path='fanpage_bot/chromedriver.exe')
+        service = Service(executable_path="fanpage_bot/chromedriver.exe")
         options = ChromeOptions()
         options.add_experimental_option("detach", True)
         options.add_argument("--disable-extensions")
@@ -33,7 +33,6 @@ class GET_USER_FFANPAGE():
         # self.browser = Login().login()
 
     def login(self):
-
         self.browser.get("https://www.facebook.com/")
         assert "Facebook" in self.browser.title
         elem = self.browser.find_element(By.ID, "email")
@@ -47,7 +46,6 @@ class GET_USER_FFANPAGE():
         return
 
     def get_user_link(self):
-
         for i, link in enumerate(self.df["link"]):
             # self.browser.get(link)
             pass
@@ -55,66 +53,90 @@ class GET_USER_FFANPAGE():
         self.browser.get("https://www.facebook.com/DoantruongQuocHoc")
         sleep(random.randint(3, 5))
 
-        self.browser.execute_script(
-            "window.scrollTo(0, document.body.scrollHeight);")
+        self.browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
         # sleep(random.randint(2,4))
         # self.browser.execute_script(
         #     "window.scrollTo(0, document.body.scrollHeight);")
 
-        for p in range(2, 5):
-            print("loop:",p)
+        l = []
+        for p in range(5, 15):
+            print("loop:", p)
             try:
                 post_react = self.browser.find_element(
-                    By.XPATH, "/html/body/div[1]/div/div[1]/div/div[3]/div/div/div/div[1]/div[1]/div/div/div[4]/div[2]/div/div[2]/div[3]/div["+str(p)+"]/div/div/div/div/div/div/div/div/div/div/div[2]/div/div/div[4]/div/div/div[1]/div/div[1]/div/div[1]/div/span/div/span[2]/span/span")
+                    By.XPATH,
+                    f"/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div/div/div[4]/div[2]/div/div[2]/div[3]/div[{p}]/div/div/div/div/div/div/div/div/div/div/div[8]/div/div/div[4]/div/div/div[1]/div/div[1]/div/div[1]/div/span/div/span[2]"
+                    # f"/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div/div/div[4]/div[2]/div/div[2]/div[3]/div[{p}]/div/div/div/div/div/div/div/div/div/div/div[8]/div/div/div[4]/div/div/div[1]/div/div[1]/div/div[1]"
+                    # "/html/body/div[1]/div/div[1]/div/div[3]/div/div/div/div[1]/div[1]/div/div/div[4]/div[2]/div/div[2]/div[3]/div["+str(p)+"]/div/div/div/div/div/div/div/div/div/div/div[2]/div/div/div[4]/div/div/div[1]/div/div[1]/div/div[1]/div/span/div/span[2]/span/span"
+                )
             except:
                 post_react = self.browser.find_element(
-                    By.XPATH, "/html/body/div[1]/div/div[1]/div/div[3]/div/div/div/div[1]/div[1]/div/div/div[4]/div[2]/div/div[2]/div[3]/div["+str(p)+"]/div/div/div/div/div/div/div/div/div/div/div[8]/div/div/div[4]/div/div/div[1]/div/div[1]/div/div[1]/div/span/div/span[2]/span/span")
+                    By.XPATH,   
+                    
+                    f"/html/body/div[1]/div/div[1]/div/div[3]/div/div/div/div[1]/div[1]/div/div/div[4]/div[2]/div/div[2]/div[3]/div[{p}]/div/div/div/div/div/div/div/div/div/div/div[8]/div/div/div[4]/div/div/div[1]/div/div[1]/div/div[1]/div/span/div/span[2]/span/span",
+                )
+
             self.actions.move_to_element(post_react).perform()
             post_react.click()
 
             sleep(random.randint(2, 4))
 
             scroll_element = self.browser.find_element(
-                By.XPATH, '/html/body/div[1]/div/div[1]/div/div[4]/div/div/div[1]/div/div[2]/div/div/div/div/div/div/div[2]/div[2]/div[3]/div/div[3]/div')
+                By.XPATH,
+                # '/html/body/div[1]/div/div[1]/div/div[4]/div/div/div[1]/div/div[2]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[3]/div'
+                # '/html/body/div[1]/div/div[1]/div/div[4]/div/div/div[1]/div/div[2]/div/div/div/div/div/div/div[2]/div[2]/div[3]/div/div[3]/div'
+                "/html/body/div[1]/div/div[1]/div/div[4]/div/div/div[1]/div/div[2]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[3]",
+            )
 
             self.actions.click_and_hold(scroll_element).move_by_offset(
-                0, 200).release().perform()
+                0, 200
+            ).release().perform()
 
             last_react_user = None
             while last_react_user is None:
-                try: 
+                try:
                     last_react_user = self.browser.find_element(
-                        By.XPATH, "/html/body/div[1]/div/div[1]/div/div[4]/div/div/div[1]/div/div[2]/div/div/div/div/div/div/div[2]/div[2]/div[3]/div/div[1]/div["+str(post_react.text)+"]/div/div/div[2]/div[1]/div/div/div/span/div/a")
-                except: 
-                
+                        By.XPATH,
+                        f"/html/body/div[1]/div/div[1]/div/div[4]/div/div/div[1]/div/div[2]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[1]/div[{post_react.text}]/div/div"
+                        # "/html/body/div[1]/div/div[1]/div/div[4]/div/div/div[1]/div/div[2]/div/div/div/div/div/div/div[2]/div[2]/div[3]/div/div[1]/div["+str(post_react.text)+"]/div/div/div[2]/div[1]/div/div/div/span/div/a"
+                    )
+                except:
                     try:
                         self.actions.click_and_hold(scroll_element).move_by_offset(
-                            0, 150).release().perform()
+                            0, 150
+                        ).release().perform()
                     except:
                         while True:
                             current_scroll_position = self.browser.execute_script(
-                                "return window.scrollY;")
+                                "return window.scrollY;"
+                            )
+                            sleep(random.randint(1,2))
                             document_height = self.browser.execute_script(
-                                "return document.body.scrollHeight;")
+                                "return document.body.scrollHeight;"
+                            )
                             if current_scroll_position <= document_height:
                                 break
 
-            l = []
-
             for i in range(1, int(post_react.text)):
                 link = self.browser.find_element(
-                    By.XPATH, "/html/body/div[1]/div/div[1]/div/div[4]/div/div/div[1]/div/div[2]/div/div/div/div/div/div/div[2]/div[2]/div[3]/div/div[1]/div["+str(i)+"]/div/div/div[2]/div[1]/div/div/div/span/div/a")
+                    By.XPATH,
+                    f"/html/body/div[1]/div/div[1]/div/div[4]/div/div/div[1]/div/div[2]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[1]/div[{i}]/div/div/div[2]/div[1]/div/div/div/span/div/a",
+                )
                 z = link.get_attribute("href")
-                z = re.sub(r'__cft__.*', '', z)
+                z = re.sub(r"__cft__.*", "", z)
                 l.append(z)
                 print(z)
 
             print(len(l))
             # close_element
             self.browser.find_element(
-                By.XPATH, "/html/body/div[1]/div/div[1]/div/div[4]/div/div/div[1]/div/div[2]/div/div/div/div/div/div/div[1]/div/div[2]/div").click()
-            sleep(random.randint(2,5))
-        
+                By.XPATH,
+                "/html/body/div[1]/div/div[1]/div/div[4]/div/div/div[1]/div/div[2]/div/div/div/div/div/div/div[1]/div/div[2]/div",
+            ).click()
+            sleep(random.randint(2, 5))
+
+        with open("user_qh_link.csv", "a", newline="", encoding="utf-8") as csvfile:
+            csv_writer = csv.writer(csvfile)
+            csv_writer.writerow(l)
         return
 
 
